4c20fcae1ea8214a9e5cfd4ba5f00b121a11c29d
Afaq2021 <afaque.aptech2021@gmail.com>
2021-11-16 19:56:29 +0500

    My Hair-Care Website

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+// Menu toggle button function start
+
+$(document).ready(function(){
+    $('.menu-toggle').click(function(){
+        $('.menu-toggle').toggleClass('active');
+    })
+})
+
+//====== Menu toggle button function End
+
+
+
+
